3205dcc5a51e4ff24c78df5da6110a4cbc6efb68
Nik Silver <nik@niksilver.com>
2016-05-21 18:45:22 +0100

    Added animation

--- Main.elm
+++ Main.elm
@@ -1,33 +1,57 @@
-import Time exposing (Time, millisecond)
+import Time exposing (Time, millisecond, second)
 
 import Html.App as App
 import Svg exposing (Svg, svg, circle)
 import Svg.Attributes exposing (width, viewBox, cx, cy, r, fill)
 
-type alias Model = Time
+import Animation exposing
+    ( Animation, animation, animate
+    , from, to, duration
+    )
+
+type alias Model =
+    { x : Float
+    , animation : Maybe Animation
+    }
 
 type Msg = Tick Time
 
 main = App.program
-    { init = (0, Cmd.none)
+    { init = (initialModel, Cmd.none)
     , update = update
     , view = view
     , subscriptions = subscriptions
     }
 
+initialModel : Model
+initialModel =
+    { x = 0
+    , animation = Nothing
+    }
+
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        Tick time -> (time, Cmd.none)
+update (Tick time) model =
+    case model.animation of
+        Nothing ->
+            let
+                anim =
+                    animation time |> from 100 |> to 300 |> duration (2 * second)
+            in
+                (Model time (Just anim), Cmd.none)
+        Just anim ->
+            (Model (animate time anim) (Just anim), Cmd.none)
 
 view : Model -> Svg Msg
 view model =
-    svg
-    [ width "500px"
-    , viewBox "0 0 500 500"
-    ]
-    [ circle [ cx "200", cy "200", r "20", fill "#aa0000" ] []
-    ]
+    let
+        x = toString model.x
+    in
+        svg
+        [ width "500px"
+        , viewBox "0 0 500 500"
+        ]
+        [ circle [ cx x, cy "200", r "20", fill "#aa0000" ] []
+        ]
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
